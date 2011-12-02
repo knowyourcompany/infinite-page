261ad76deb182fb0c0e570394088032cce274a4c
Javan Makhmali <javan@javan.us>
2011-12-02 18:00:44 -0500

    add current_page method/helper

--- lib/infinite_page.rb
+++ lib/infinite_page.rb
@@ -4,6 +4,10 @@ module InfinitePage
       ActiveSupport.on_load :active_record do
         ActiveRecord::Base.send :include, InfinitePage::Scope
       end
+
+      ActiveSupport.on_load :action_controller do
+        ActionController::Base.send :include, InfinitePage::CurrentPage
+      end
     end
   end
 
@@ -13,6 +17,18 @@ module InfinitePage
     module ClassMethods
       def page(page, per_page = 50)
         limit(per_page).offset((page - 1) * per_page)
+      end
+    end
+  end
+
+  module CurrentPage
+    extend ActiveSupport::Concern
+
+    included { helper_method :current_page }
+
+    module InstanceMethods
+      def current_page
+        (params[:page] || 1).to_i
       end
     end
   end
