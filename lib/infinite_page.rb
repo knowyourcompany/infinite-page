53b73a14b32e161285861e807241eb2b54c2a571
Javan Makhmali <javan@javan.us>
2011-12-02 18:13:53 -0500

    drop deprecated InstanceMethods module

--- lib/infinite_page.rb
+++ lib/infinite_page.rb
@@ -26,10 +26,8 @@ module InfinitePage
 
     included { helper_method :current_page }
 
-    module InstanceMethods
-      def current_page
-        (params[:page] || 1).to_i
-      end
+    def current_page
+      (params[:page] || 1).to_i
     end
   end
 end
